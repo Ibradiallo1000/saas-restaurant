c11cead98cf3cb2ddc72baaf50d1339eefc896db
ibrahim diallo <dialloibra1000@gmail.com>
2026-02-14 19:43:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7907252579-dd6af",
+  "appId": "1:136010512437:web:e26ac594114f9b5c86597c",
+  "apiKey": "AIzaSyBOupmRoeVRg6uuW7HvHTuvFJfSsvDvDLs",
+  "authDomain": "studio-7907252579-dd6af.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "136010512437"
+};
